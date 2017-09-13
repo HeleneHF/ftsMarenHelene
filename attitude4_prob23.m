05e3c51b52f6af8eff0b7d99a89ab1b9b8003e62
HeleneHF <helenehf@stud.ntnu.no>
2017-09-13 23:39:43 +0200

    update

---
+++ attitude4_prob23.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Name:         Ass1 Problem 2                                  %
+% Assignment:   Assignment 1 Part 2                                 %
+% Author:       2017-09-13 Helene H.Fossum                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Problem 2.3
+% constants
+deg2rad = pi/180;   
+rad2deg = 180/pi;
+
+% model parameters
+R = 100;    % [m] radius of the circle
+U = 1.5;    % [m/s] speed without current
+U_c = 0.6;  % [m/s] speed of the ocean current
+omega = 0.015; % from problem 2.2
+
+phi = 0;    % [degree] roll angle
+tetha = 2;  % [degree] pitch angle
+psi = 30;   % [degree] yaw angle
+
+alpha_c = 10; % [degree]
+beta_c = 45;  % [degree]  
+
